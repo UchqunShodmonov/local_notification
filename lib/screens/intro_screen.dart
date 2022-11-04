51c67862100b4ad8692ad4770d8cf4313b3a70f9
uchqun <shodmonov.uchqun@gmail.com>
2022-11-04 11:14:26 +0500

    inti

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -3,8 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:globo_fitness/screens/notification_payload_screen.dart';
 import 'package:globo_fitness/services/local_notification_service.dart';
-import '../shared/menu_drawer.dart';
-import '../shared/menu_bottom.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({Key? key}) : super(key: key);
@@ -26,90 +24,62 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('AAAAAAA')),
-        //appBar: AppBar(title: Text('Smetto di fumare')),
-        drawer: MenuDrawer(),
-        bottomNavigationBar: MenuBottom(),
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
+      appBar: AppBar(
+        title: Text(
+          'Local Notification',
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
             image: AssetImage('assets/dog.jpg'),
-            //image: AssetImage('assets/20_agosto.jpeg'),
             fit: BoxFit.cover,
-          )),
-          child: Center(
-            /*
-                child: Container(
-                  padding: EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Colors.white70
-                  ),
-                  child: Text(
-                    'AAAAAAAAAA,\nAAAAAAAAA',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 22,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      shadows: [
-                        Shadow(
-                          offset: Offset(1.0, 1.0),
-                          blurRadius: 2.0,
-                          color: Color.fromARGB(255, 30, 169, 233)
-                        )
-                      ],
-                    )),
-                    */
-            child: SizedBox(
-              height: 300,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () async {
-                        service.showNotification(
-                            id: 0,
-                            title: 'Notification Title',
-                            body: 'Some boby');
-                      },
-                      child: const Text('Show Local Notification')),
-                  ElevatedButton(
-                      onPressed: () async {
-                        service.showScheduledNotification(
-                            id: 1,
-                            title: 'Notification Title',
-                            body: 'Some boby',
-                            seconds: 4);
-                      },
-                      child: const Text('Show Scheduled Notification')),
-                  ElevatedButton(
-                      onPressed: () async {
-                        service.showNotificationWithPayload(
-                            id: 2,
-                            title: 'Notification Title',
-                            body: 'Some boby',
-                            payload: 'Payload Navigation');
-                      },
-                      child: const Text('Show Notification with Payload')),
-                ],
-              ),
-            ),
           ),
-        ));
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                service.showNotification(
+                  id: 0,
+                  title: 'Notification Title',
+                  body: 'Some boby',
+                );
+              },
+              child: const Text('Show Local Notification'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                service.showNotificationWithPayload(
+                  id: 2,
+                  title: 'Notification Title',
+                  body: 'Some boby',
+                  payload: 'Payload Navigation',
+                );
+              },
+              child: const Text('Show Notification with Payload'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  void listenToNotification() =>
-      service.onNotificationClick.stream.listen(onNotificationListener);
+  void listenToNotification() {
+    service.onNotificationClick.stream.listen(onNotificationListener);
+  }
 
   void onNotificationListener(String? payload) {
     if (payload != null && payload.isNotEmpty) {
-      //print('payload $payload');
-
       Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NotificationPayloadScreen(payload: payload))
-          );
+        context,
+        MaterialPageRoute(
+          builder: (context) => NotificationPayloadScreen(payload: payload),
+        ),
+      );
     }
   }
 }
--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -1,13 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
+
 import 'package:rxdart/rxdart.dart';
-
-/*
-source: https://youtu.be/Yrq2llD2Ldw
-https://pub.dev/packages/flutter_local_notifications > iOS Setup > Copy "Swift code" > GlOBO_FITNESS/ios/Runner/AppDelegate.swift > paste code under "GeneratedPluginRegistrant.register(with: self)"
-
-*/
 
 class LocalNotificationService {
   LocalNotificationService();
@@ -16,8 +9,6 @@ class LocalNotificationService {
   final BehaviorSubject<String?> onNotificationClick = BehaviorSubject();
 
   Future<void> init() async {
-    tz.initializeTimeZones();
-    // paste icons folder under GlOBO_FITNESS/android/app/main/res
     const AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings('@drawable/ic_stat_android');
 
@@ -30,49 +21,44 @@ class LocalNotificationService {
     );
 
     final InitializationSettings settings = InitializationSettings(
-        android: androidInitializationSettings, iOS: iOSInitializationSettings);
+      android: androidInitializationSettings,
+      iOS: iOSInitializationSettings,
+    );
 
-    await _localNotificationService.initialize(settings,
-        onSelectNotification: onSelectNotification);
+    await _localNotificationService.initialize(
+      settings,
+      onSelectNotification: onSelectNotification,
+    );
   }
 
   Future<NotificationDetails> _notificationDetails() async {
     const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails('channelId', 'channelName',
-            channelDescription: 'description',
-            importance: Importance.max,
-            priority: Priority.max,
-            playSound: true);
+        AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      channelDescription: 'description',
+      importance: Importance.max,
+      priority: Priority.max,
+      playSound: true,
+    );
     const IOSNotificationDetails iosNotificationDetails =
         IOSNotificationDetails();
 
     return const NotificationDetails(
-        android: androidNotificationDetails, iOS: iosNotificationDetails);
+      android: androidNotificationDetails,
+      iOS: iosNotificationDetails,
+    );
   }
 
   Future<void> showNotification(
       {required int id, required String title, required String body}) async {
     final deatails = await _notificationDetails();
-    await _localNotificationService.show(id, title, body, deatails);
-  }
-
-  Future<void> showScheduledNotification({
-    required int id,
-    required String title,
-    required String body,
-    required int seconds,
-  }) async {
-    final deatails = await _notificationDetails();
-    await _localNotificationService.zonedSchedule(
-        id,
-        title,
-        body,
-        tz.TZDateTime.from(
-            DateTime.now().add(Duration(seconds: seconds)), tz.local),
-        deatails,
-        androidAllowWhileIdle: true,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime);
+    await _localNotificationService.show(
+      id,
+      title,
+      body,
+      deatails,
+    );
   }
 
   Future<void> showNotificationWithPayload(
@@ -81,17 +67,19 @@ class LocalNotificationService {
       required String body,
       required String payload}) async {
     final deatails = await _notificationDetails();
-    await _localNotificationService.show(id, title, body, deatails,
-        payload: payload);
+    await _localNotificationService.show(
+      id,
+      title,
+      body,
+      deatails,
+      payload: payload,
+    );
   }
 
   void _onDidReceiveLocalNotification(
-      int id, String? title, String? body, String? payload) {
-    //print('id $id');
-  }
+      int id, String? title, String? body, String? payload) {}
 
   void onSelectNotification(String? payload) {
-    //print('AAAAA $payload');
     if (payload != null && payload.isNotEmpty) {
       onNotificationClick.add(payload);
     }
